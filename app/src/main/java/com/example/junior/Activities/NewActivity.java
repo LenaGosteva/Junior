50008a022dd52524870c23287e3dbb50c714176b
LenaGosteva <len0k.goz@gmail.com>
2023-11-12 10:10:11 +0300

    Переход на один сплошной скролл

--- app/src/main/java/com/example/junior/Activities/NewActivity.java
+++ app/src/main/java/com/example/junior/Activities/NewActivity.java
@@ -1,11 +1,8 @@
 package com.example.junior.Activities;
 
-import android.app.Activity;
 import android.os.Bundle;
 
-import com.example.junior.Adapters.DocumentAdapter;
 import com.example.junior.Classes.UsersDocument;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -22,8 +19,6 @@ public class NewActivity extends AppCompatActivity {
 
         binding = ActivityNewBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        binding.viewPager.setAdapter(new DocumentAdapter(getSupportFragmentManager(), NewActivity.this));
-        binding.tabs.setupWithViewPager(binding.viewPager);
 
 
     }
